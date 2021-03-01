76e18c68c7bf1e46516ce580395b108ac6f7a327
Justin Oberg <oberg218@morris.umn.edu>
2021-03-01 17:02:08 -0600

    Create test for getTodo() in todoService.ts
    Created a test for getTodos, generated getTodos() method,

--- client/src/app/todos/todos.service.spec.ts
+++ client/src/app/todos/todos.service.spec.ts
@@ -1,16 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Todo } from './todo';
 
 import { TodosService } from './todos.service';
 
 describe('TodosService', () => {
-  let service: TodosService;
 
+  const testTodos: Todo[] = [
+    // {
+    //   _id: '1'
+    // }
+  ];
+  let todoService: TodosService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TodosService);
+    TestBed.configureTestingModule({imports:
+      [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    todoService = TestBed.inject(TodosService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(todoService).toBeTruthy();
+  });
+
+  it('getTodos() call api/todos', () => {
+    todoService.getTodos().subscribe(
+      todos => expect(todos).toEqual(testTodos)
+    );
+
+    const req = httpTestingController.expectOne(todoService.todoUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(testTodos);
   });
 });
--- client/src/app/todos/todos.service.ts
+++ client/src/app/todos/todos.service.ts
@@ -1,13 +1,21 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
+import { Todo } from './todo';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TodosService {
-
+export class TodosService{
+  readonly todoUrl: string = environment.apiUrl + 'todos';
   constructor(private httpClient: HttpClient) { }
+  getTodos(): Observable<Todo[]> {
+    let httpParams: HttpParams = new HttpParams();
+    const todos: Todo[] = [];
+    return this.httpClient.get<Todo[]>(this.todoUrl, {
+      params: httpParams,
+  });
+}
 }
