f6dbe3b63e66f1a0e96ffdcfb5ee5105fd94c5d0
Justin Oberg <oberg218@morris.umn.edu>
2021-03-01 19:52:09 -0600

    Added beforeAll setup to TodoControllerSpec.java
    Added beforeAll for testing purposes in TodoControllerSpec.java
    and made assignments to member variables. Also declared
    imports.

--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -1,5 +1,50 @@
 package umm3601.todo;
 
+import java.io.IOException;
+import java.util.Arrays;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mockrunner.mock.web.MockHttpServletRequest;
+import com.mockrunner.mock.web.MockHttpServletResponse;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.types.ObjectId;
+import org.junit.jupiter.api.BeforeAll;
+
+/**
+ * Tests the logic of the TodoController
+ *
+ * @throws IOException
+ */
 public class TodoControllerSpec {
+
+  MockHttpServletRequest mockReq = new MockHttpServletRequest();
+  MockHttpServletResponse mockRes = new MockHttpServletResponse();
+
+  private TodoController todoController;
+
+  static MongoClient mongoClient;
+  static MongoDatabase db;
+
+  static ObjectMapper jsonMapper = new ObjectMapper();
+
+  @BeforeAll
+  public static void setupAll() {
+    String mongoAddr = System.getenv().getOrDefault("MONGO_ADDR", "localhost");
+
+    mongoClient = MongoClients.create(
+    MongoClientSettings.builder()
+    .applyToClusterSettings(builder ->
+    builder.hosts(Arrays.asList(new ServerAddress(mongoAddr))))
+    .build());
+
+    db = mongoClient.getDatabase("test");
+
+  }
+
 
 }
