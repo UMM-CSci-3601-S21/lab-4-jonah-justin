e67b41e2bfeb79810febf91ddf470771fea27529
Justin Oberg <oberg218@morris.umn.edu>
2021-03-01 20:09:06 -0600

    Add @BeforeEach to TodoControllerSpec
    Added @BeforeEach and setup before directions for TodoController
    testing. However, no tests have been created yet.

--- server/src/main/java/umm3601/todo/TodoController.java
+++ server/src/main/java/umm3601/todo/TodoController.java
@@ -1,5 +1,10 @@
 package umm3601.todo;
 
+import com.mongodb.client.MongoDatabase;
+
 public class TodoController {
+
+	public TodoController(MongoDatabase db) {
+	}
 
 }
--- server/src/test/java/umm3601/todo/TodoControllerSpec.java
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -1,7 +1,9 @@
 package umm3601.todo;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mockrunner.mock.web.MockHttpServletRequest;
@@ -10,10 +12,13 @@ import com.mongodb.MongoClientSettings;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import org.bson.Document;
 import org.bson.types.ObjectId;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 /**
  * Tests the logic of the TodoController
@@ -26,6 +31,7 @@ public class TodoControllerSpec {
   MockHttpServletResponse mockRes = new MockHttpServletResponse();
 
   private TodoController todoController;
+  private ObjectId eefsId;
 
   static MongoClient mongoClient;
   static MongoDatabase db;
@@ -43,7 +49,49 @@ public class TodoControllerSpec {
     .build());
 
     db = mongoClient.getDatabase("test");
+  }
 
+  @BeforeEach
+  public void setupEach() throws IOException {
+    //Reset our mock request and response objects.
+    mockReq.resetAll();
+    mockRes.resetAll();
+
+    //Setup Database.
+    MongoCollection<Document> todoDocuments = db.getCollection("todos");
+    todoDocuments.drop();
+    List<Document> testTodos = new ArrayList<>();
+    testTodos.add(
+      new Document()
+      .append("owner", "Phil")
+      .append("status", false)
+      .append("category", "videogame")
+      .append("body","lorem impsum sortum"));
+    testTodos.add(
+      new Document()
+      .append("owner", "John")
+      .append("status", true)
+      .append("category", "projects")
+      .append("body", "ipsum loruem phospate buillion"));
+    testTodos.add(
+      new Document()
+      .append("owner", "Mark")
+      .append("status", false)
+      .append("category", "videogame")
+      .append("body", "projected use up 3%"));
+  eefsId = new ObjectId();
+  Document eef =
+    new Document()
+    .append("_id", eefsId)
+    .append("name", "Eef")
+    .append("status", true)
+    .append("category", "projects")
+    .append("body", "create new project");
+
+  todoDocuments.insertMany(testTodos);
+  todoDocuments.insertOne(eef);
+
+  todoController = new TodoController(db);
   }
 
 
