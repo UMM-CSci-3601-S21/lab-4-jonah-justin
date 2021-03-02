9eb61d81bace9d4b889ce3756b80454df42948a5
Justin Oberg <oberg218@morris.umn.edu>
2021-03-02 14:30:29 -0600

    Add content to todos-list template
    Added content to todos-list template.

--- client/src/app/todos/todos-list.component.ts
+++ client/src/app/todos/todos-list.component.ts
@@ -1,13 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Todo, TodoStatus } from './todo';
+import { TodosService } from './todos.service';
 
 @Component({
   selector: 'app-todos-list',
   templateUrl: './todos-list.component.html',
   styleUrls: ['./todos-list.component.scss']
 })
-export class TodosListComponent implements OnInit {
+export class TodosListComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public serverFilteredTodos: Todo[];
+  public filteredTodo: Todo[];
+  public todoOwner: string;
+  public todoBody: string;
+  public todoCategory: string;
+  public todoStatus: TodoStatus;
+  getTodosSub: Subscription;
+
+  constructor(private todosService: TodosService) {
+
+  }
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
   }
